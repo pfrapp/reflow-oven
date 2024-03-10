8d5727fe4f527f40eed93399b4009a32e94e3b53
Philipp Rapp <philipprapp@gmx.de>
2024-03-10 17:23:59 +0100

    Added first test program for the reflow controller

---
+++ tiva/reflow_ctrl/main.c
@@ -0,0 +1,78 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+
+
+//*****************************************************************************
+//
+// Internal functions.
+//
+//*****************************************************************************
+static void turnOnRedLed() {
+    // Red: PF1
+    uint8_t pin = GPIO_PIN_1;
+    GPIOPinWrite(GPIO_PORTF_BASE, pin, pin);
+}
+
+static void turnOffRedLed() {
+    uint8_t pin = GPIO_PIN_1;
+    GPIOPinWrite(GPIO_PORTF_BASE, pin, 0);
+}
+
+static void turnOnGreenLed() {
+    // Green: PF3
+    uint8_t pin = GPIO_PIN_3;
+    GPIOPinWrite(GPIO_PORTF_BASE, pin, pin);
+}
+
+static void turnOffGreenLed() {
+    uint8_t pin = GPIO_PIN_3;
+    GPIOPinWrite(GPIO_PORTF_BASE, pin, 0);
+}
+
+static void turnOnBlueLed() {
+    // Blue: PF2
+    uint8_t pin = GPIO_PIN_2;
+    GPIOPinWrite(GPIO_PORTF_BASE, pin, pin);
+}
+
+static void turnOffBlueLed() {
+    uint8_t pin = GPIO_PIN_2;
+    GPIOPinWrite(GPIO_PORTF_BASE, pin, 0);
+}
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    //
+    // Enable the GPIO pins for the LED (PF2 & PF3).
+    // Green: PF3
+    // Blue: PF2
+    // Red: PF1
+    //
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1);
+    turnOffRedLed();
+    turnOffGreenLed();
+    turnOffBlueLed();
+
+    while(true) {
+        SysCtlDelay(SysCtlClockGet() / 2);
+        turnOnBlueLed();
+        SysCtlDelay(SysCtlClockGet() / 2);
+        turnOffBlueLed();
+    }
+
+
+	return 0;
+}
