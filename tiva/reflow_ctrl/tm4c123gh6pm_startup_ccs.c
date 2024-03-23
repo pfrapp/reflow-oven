22ad09ab864e034c414a20526f03989a5089400b
Philipp Rapp <philipprapp@gmx.de>
2024-03-23 23:08:17 +0100

    Added USB on the Tiva
    Added USB code, includes, library, and interrupt handler.

--- tiva/reflow_ctrl/tm4c123gh6pm_startup_ccs.c
+++ tiva/reflow_ctrl/tm4c123gh6pm_startup_ccs.c
@@ -34,6 +34,9 @@ static void NmiSR(void);
 static void FaultISR(void);
 static void IntDefaultHandler(void);
 
+// USB interrupt handler
+extern void USB0DeviceIntHandler(void);
+
 //*****************************************************************************
 //
 // External declaration for the reset handler that is to be called when the
@@ -127,7 +130,7 @@ void (* const g_pfnVectors[])(void) =
     0,                                      // Reserved
     0,                                      // Reserved
     IntDefaultHandler,                      // Hibernate
-    IntDefaultHandler,                      // USB0
+    USB0DeviceIntHandler,                   // USB0
     IntDefaultHandler,                      // PWM Generator 3
     IntDefaultHandler,                      // uDMA Software Transfer
     IntDefaultHandler,                      // uDMA Error
